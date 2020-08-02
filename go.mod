297795e94af71b971184589a6332b53f7eb0939b
wenchyzhu <wenchyzhu@gmail.com>
2020-08-02 18:46:25 +0800

    feat: optimize options definition

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/Wenchy/tableau/testpb v0.0.0-00010101000000-000000000000
+	github.com/tealeg/xlsx v1.0.5
+	github.com/tealeg/xlsx/v3 v3.2.0
 	google.golang.org/protobuf v1.25.0
 )
 
--- main.go
+++ main.go
@@ -4,30 +4,59 @@ import (
 	"fmt"
 
 	"github.com/Wenchy/tableau/testpb"
+	"github.com/tealeg/xlsx/v3"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/descriptorpb"
 )
+
+const tableauPackageName = "test"
 
 func main() {
 	// fmt.Println("Hello, world.")
 	// item := testpb.Item{}
 	// Redact(item.ProtoReflect().Interface())
 
-	testConf := testpb.TestConf{}
-	Redact(testConf.ProtoReflect().Descriptor())
+	itemConf := testpb.ItemConf{}
+	md := itemConf.ProtoReflect().Descriptor()
+
+	ParseFileOptions(md.ParentFile())
+	fmt.Println("==================")
+	ParseMessageOptions(md)
+	fmt.Println("==================")
+	ParseFieldOptions(md)
+	fmt.Println("==================")
+
+	readSheet("tests/Test.xlsx")
 }
 
-// Redact a pb message
-func Redact(md protoreflect.MessageDescriptor) {
+// ParseFileOptions is aimed to parse the options of a protobuf definition file.
+func ParseFileOptions(fd protoreflect.FileDescriptor) {
+	opts := fd.Options().(*descriptorpb.FileOptions)
+	workbook := proto.GetExtension(opts, testpb.E_Workbook).(string)
+	fmt.Printf("file:%s.proto, workbook:%s\n", fd.FullName(), workbook)
+}
+
+// ParseMessageOptions is aimed to parse the options of a protobuf message.
+func ParseMessageOptions(md protoreflect.MessageDescriptor) {
+	opts := md.Options().(*descriptorpb.MessageOptions)
+	worksheet := proto.GetExtension(opts, testpb.E_Worksheet).(string)
+	metarow := proto.GetExtension(opts, testpb.E_Metarow).(int32)
+	descrow := proto.GetExtension(opts, testpb.E_Descrow).(int32)
+	datarow := proto.GetExtension(opts, testpb.E_Datarow).(int32)
+	fmt.Printf("message:%s, worksheet:%s, metarow:%d, descrow:%d, datarow:%d\n", md.FullName(), worksheet, metarow, descrow, datarow)
+}
+
+// ParseFieldOptions is aimed to parse the options of all the fields of a protobuf message.
+func ParseFieldOptions(md protoreflect.MessageDescriptor) {
 	for i := 0; i < md.Fields().Len(); i++ {
 		fd := md.Fields().Get(i)
-		if fd.ParentFile().Package() != "test" {
+		if fd.ParentFile().Package() != tableauPackageName {
 			return
 		}
 		if fd.Kind() == protoreflect.MessageKind {
 			fmt.Println(fd.Cardinality().String(), fd.Kind().String(), fd.FullName(), fd.Number())
-			Redact(fd.Message())
+			ParseFieldOptions(fd.Message())
 		}
 
 		// if fd.IsList() {
@@ -37,7 +66,7 @@ func Redact(md protoreflect.MessageDescriptor) {
 		opts := fd.Options().(*descriptorpb.FieldOptions)
 		col := proto.GetExtension(opts, testpb.E_Col).(string)
 		etype := proto.GetExtension(opts, testpb.E_Type).(testpb.TableauFieldType)
-		fmt.Printf("%s, %s %s %s = %d [(col) = \"%s\", (type) = %s];\n", fd.ParentFile().Package(), fd.Cardinality().String(), fd.Kind().String(), fd.FullName().Name(), fd.Number(), col, etype.String())
+		fmt.Printf("%s %s %s = %d [(col) = \"%s\", (type) = %s];\n", fd.Cardinality().String(), fd.Kind().String(), fd.FullName().Name(), fd.Number(), col, etype.String())
 		// fmt.Println(fd.ContainingMessage().FullName())
 
 	}
@@ -51,4 +80,19 @@ func Redact(md protoreflect.MessageDescriptor) {
 	// 	}
 	// 	return true
 	// })
+}
+
+func readSheet(workbook string) {
+	// open an existing file
+	wb, err := xlsx.OpenFile(workbook)
+	if err != nil {
+		panic(err)
+	}
+	// wb now contains a reference to the workbook
+	// show all the sheets in the workbook
+	fmt.Println("Sheets in this file:")
+	for i, sh := range wb.Sheets {
+		fmt.Println(i, sh.Name)
+	}
+	fmt.Println("----")
 }
