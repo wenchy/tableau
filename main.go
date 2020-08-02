7487b8ddf7575b80166fff14e8c4efb0bc75db7f
wenchyzhu <wenchyzhu@gmail.com>
2020-08-02 08:12:51 +0800

    feat: traverse

--- main.go
+++ main.go
@@ -11,24 +11,46 @@ import (
 
 func main() {
 	// fmt.Println("Hello, world.")
-	// Redact clears every sensitive field in pb.
-	// var item testpb.Item
-	item := testpb.Item{}
-	item.Id = 1
-	item.Num = 2
-	Redact(item.ProtoReflect().Interface())
+	// item := testpb.Item{}
+	// Redact(item.ProtoReflect().Interface())
+
+	testConf := testpb.TestConf{}
+	Redact(testConf.ProtoReflect().Interface())
 }
 
 // Redact a pb message
 func Redact(pb proto.Message) {
 	m := pb.ProtoReflect()
-	m.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+
+	for i := 0; i < m.Descriptor().Fields().Len(); i++ {
+		fd := m.Descriptor().Fields().Get(i)
+		if fd.ParentFile().Package() != "test" {
+			return
+		}
+		if fd.Kind() == protoreflect.MessageKind {
+			fmt.Println(fd.Cardinality().String(), fd.Kind().String(), fd.FullName(), fd.Number())
+			Redact(fd.Options())
+		}
+
+		// if fd.IsList() {
+		// 	fmt.Println("repeated", fd.Kind().String(), fd.FullName().Name())
+		// 	// Redact(fd.Options().ProtoReflect().Interface())
+		// }
 		opts := fd.Options().(*descriptorpb.FieldOptions)
 		col := proto.GetExtension(opts, testpb.E_Col).(string)
-		if col != "" {
-			fmt.Println(fd.FullName().Name(), col)
-			// fmt.Println(fd.ContainingMessage().FullName())
-		}
-		return true
-	})
+		etype := proto.GetExtension(opts, testpb.E_Type).(testpb.TableauFieldType)
+		fmt.Printf("%s, %s %s %s = %d [(col) = \"%s\", (type) = %s];\n", fd.ParentFile().Package(), fd.Cardinality().String(), fd.Kind().String(), fd.FullName().Name(), fd.Number(), col, etype.String())
+		// fmt.Println(fd.ContainingMessage().FullName())
+
+	}
+
+	// m.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+	// 	opts := fd.Options().(*descriptorpb.FieldOptions)
+	// 	col := proto.GetExtension(opts, testpb.E_Col).(string)
+	// 	if col != "" {
+	// 		fmt.Println(fd.FullName().Name(), col)
+	// 		// fmt.Println(fd.ContainingMessage().FullName())
+	// 	}
+	// 	return true
+	// })
 }
