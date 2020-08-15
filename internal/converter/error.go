906fa345834db4d7668a8d2e0fe192d2284db5cd
wenchyzhu <wenchyzhu@gmail.com>
2020-08-15 12:32:49 +0800

    fix: add missing error.go

---
+++ internal/converter/error.go
@@ -0,0 +1,27 @@
+package converter
+
+import (
+	"bytes"
+	"html/template"
+)
+
+const tmplName = "test"
+
+type IllegalFieldType struct {
+	FieldType string
+	Line      int
+}
+
+const ErrorTmpl_IllegalFieldType = "{{.FieldType}} is illegal at {{.Line}}"
+
+func (e IllegalFieldType) Error() string {
+	tmpl, err := template.New(tmplName).Parse(ErrorTmpl_IllegalFieldType)
+	if err != nil {
+		panic(err)
+	}
+	var errmsg bytes.Buffer
+	if err := tmpl.Execute(&errmsg, e); err != nil {
+		panic(err)
+	}
+	return errmsg.String()
+}
